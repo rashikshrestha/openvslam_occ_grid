2807142679973ac6ee335d32f78db1917faecdd1
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2020-09-22 16:14:24 +0545

    initial commit

---
+++ plotter.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+
+import sys
+import msgpack
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from scipy.spatial.transform import Rotation as R
+from matplotlib.widgets import Slider
+
+
+class Plotter:
+	def __init__(self, file_name):
+		print("Initialized")
+		# Read msg pack
+		with open(file_name, "rb") as msg_pack_file:
+		    msg_pack_byte_data = msg_pack_file.read()
+
+		self.data = msgpack.unpackb(msg_pack_byte_data)
+
+		# Settings for plotting
+		plt.figure(1, figsize=[20,10])
+		self.ax_3d = plt.axes([0,0.2,0.5,0.8],projection='3d')
+		# self.ax_3d.set_xlim(-2, 2)
+		# self.ax_3d.set_ylim(-2, 2)
+		# self.ax_3d.set_zlim(-5, 5)
+		self.ax_3d.set_xlabel('X axis')
+		self.ax_3d.set_ylabel('Y axis')
+		self.ax_3d.set_zlabel('Z axis')
+
+		self.ax_occ_grid	= 	plt.axes([0.5,0.2,0.45,0.75])
+		self.ax_occ_grid.set_xlim(-10,10)
+		self.ax_occ_grid.set_ylim(-4,16)
+		self.ax_occ_grid.set_xlabel('X axis')
+		self.ax_occ_grid.set_ylabel('Y axis')
+
+		
+	def rotation_matrix(self, axis, theta):
+	    """
+	    Return the rotation matrix associated with counterclockwise rotation about
+	    the given axis by theta radians.
+	    """
+	    axis = np.asarray(axis)
+	    axis = axis / np.sqrt(np.dot(axis, axis))
+	    a = np.cos(theta / 2.0)
+	    b, c, d = -axis * np.sin(theta / 2.0)
+	    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+	    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+	    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+	                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+	                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+	def rotate(self, axis, angle, points):
+		"""
+		Rotate 3d coordinates
+		"""
+		rad_angle = (angle/180)*np.pi
+		points = np.transpose(points)
+		points = np.dot(self.rotation_matrix(axis, rad_angle), points)
+		points = np.transpose(points)
+		return points
+
+	def quaternion_to_euler(quad):
+	    """
+	    Converts quaternions with components w, x, y, z into a  roll, pitch, yaw
+	    """
+	    w = quad[0]
+	    x = quad[1]
+	    y = quad[2]
+	    z = quad[3]
+	    sinr_cosp = 2 * (w * x + y * z)
+	    cosr_cosp = 1 - 2 * (x**2 + y**2)
+	    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+	    sinp = 2 * (w * y - z * x)
+	    pitch = np.where(np.abs(sinp) >= 1,
+	                     np.sign(sinp) * np.pi / 2,
+	                     np.arcsin(sinp))
+
+	    siny_cosp = 2 * (w * z + x * y)
+	    cosy_cosp = 1 - 2 * (y**2 + z**2)
+	    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+	    eul = [roll, pitch, yaw]
+	    return eul
+
+	def extract_keyframe_poses(self):
+		"""
+		Extract keyframe poses
+		"""
+		kfs_trans = []
+		kfs_rot = []
+		kfs_pos = []
+		for kf in self.data['keyframes']:
+			ktrans = self.data['keyframes'][kf]['trans_cw']
+			kf_trans = [ktrans[0],ktrans[1],ktrans[2]]
+			
+			krot = self.data['keyframes'][kf]['rot_cw']
+			kf_rot = [krot[0],krot[1],krot[2],krot[3]]
+			
+			kf_rot_mat = R.from_quat(kf_rot)
+			kf_pos = np.matmul(-np.transpose(kf_rot_mat.as_matrix()), np.transpose(kf_trans))
+			# print(kf_trans, kf_pos)
+
+			kfs_trans.append(kf_trans)
+			kfs_rot.append(kf_rot)
+			kfs_pos.append(kf_pos)
+
+
+
+		self.keyframes_trans = np.array(kfs_trans)
+		self.keyframes_rot = np.array(kfs_rot)
+		self.keyframes_pos = np.array(kfs_pos)
+
+	def extract_landmark_poses(self):
+		"""
+		Extract landmark poses
+		"""
+		lms_trans = []
+		for lm in self.data['landmarks']:
+			lmtrans = self.data['landmarks'][lm]['pos_w']
+			lm_trans = [lmtrans[0],lmtrans[1],lmtrans[2]]
+			lms_trans.append(lm_trans)
+
+		self.landmarks_trans = np.array(lms_trans)
+
+	def rotate_keypoints_and_landmarks(self, x_angle, y_angle, z_angle):
+
+		self.keyframes_pos = self.rotate([1,0,0],x_angle,self.keyframes_pos)
+		self.landmarks_trans = self.rotate([1,0,0],x_angle,self.landmarks_trans)
+
+		self.keyframes_pos = self.rotate([0,1,0],y_angle,self.keyframes_pos)
+		self.landmarks_trans = self.rotate([0,1,0],y_angle,self.landmarks_trans)
+
+		self.keyframes_pos = self.rotate([0,0,1],z_angle,self.keyframes_pos)
+		self.landmarks_trans = self.rotate([0,0,1],z_angle,self.landmarks_trans)
+
+	
+
+	def height_thresholder(self, points, vmin, vmax):
+		new_points = []
+		for point in points:
+			if point[2] > vmin and point[2] < vmax:
+				new_points.append(point)
+
+		return np.array(new_points)
+
+	def apply_height_threshold(self, lower_threshold, upper_threshold):
+
+		v_min = np.amin(self.landmarks_trans, axis=0)[2]
+		v_max = np.amax(self.landmarks_trans, axis=0)[2]
+		range_ = v_max-v_min
+
+		print('Min Z = ', v_min)
+		print('Max Z = ', v_max)
+
+		upper_val = v_min + ((100-upper_threshold)/100)*range_
+		lower_val = v_min + (lower_threshold/100)*range_
+
+		self.landmarks_trans = self.height_thresholder(self.landmarks_trans,lower_val,upper_val)
+
+
+
+	def get_path_mask(self, coordinate, radius):
+	
+		minx = self.landmarks_trans[:,0] < (coordinate[0]-radius)
+		maxx = self.landmarks_trans[:,0] > (coordinate[0]+radius)
+		miny = self.landmarks_trans[:,1] < (coordinate[1]-radius)
+		maxy = self.landmarks_trans[:,1] > (coordinate[1]+radius)
+
+		x_filter = np.logical_or(minx,maxx)
+		y_filter = np.logical_or(miny,maxy)
+		all_filter = np.logical_or(x_filter,y_filter)
+
+		return all_filter
+
+	def apply_path_threshold(self, radius):
+
+		print("Applying path threshold... ")
+
+		all_mask = self.get_path_mask(self.keyframes_pos[0],radius)
+
+		for kp in self.keyframes_pos:
+			mask = self.get_path_mask(kp,radius)
+			all_mask = np.logical_and(mask,all_mask)
+
+		self.landmarks_trans = self.landmarks_trans[all_mask]
+
+	def plot_keyframes(self):
+
+		# self.ax_3d.scatter3D(self.keyframes_trans[:,0],self.keyframes_trans[:,1],self.keyframes_trans[:,2],color="r", s=4);
+		# self.ax_occ_grid.scatter(self.keyframes_trans[:,0],self.keyframes_trans[:,1], color="r", s=4);
+
+		self.ax_3d.scatter3D(self.keyframes_pos[:,0],self.keyframes_pos[:,1],self.keyframes_pos[:,2],color="blue", s=4);
+
+		self.ax_occ_grid.scatter(self.keyframes_pos[:,0],self.keyframes_pos[:,1], color="blue", s=2);
+
+	def plot_landmarks(self):
+
+		self.ax_3d.scatter3D(self.landmarks_trans[:,0],self.landmarks_trans[:,1],self.landmarks_trans[:,2],color="black");
+		self.ax_occ_grid.scatter(self.landmarks_trans[:,0],self.landmarks_trans[:,1], color="black", s=4);
+
+	def animate_keyframes(self, delay):
+		i=0
+		for point in self.keyframes_pos:
+			i += 1
+			self.ax_occ_grid.scatter(point[0], point[1], color="red");
+			self.ax_occ_grid.text(point[0], point[1], i)
+
+			self.ax_3d.scatter3D(point[0], point[1], point[2], color="red");
+			self.ax_3d.text(point[0], point[1], point[2], i)
+			plt.pause(delay)
+			# if i>50:
+			# 	break
+
+
+
+
+if __name__ == "__main__": 
+
+	if len(sys.argv) != 2:
+		print('Implementation:   python3 plotter.py /path/to/msgpack/file.msg')
+		sys.exit()
+    
+	p = Plotter(sys.argv[1])
+
+	p.extract_keyframe_poses()
+	p.extract_landmark_poses()
+
+	p.rotate_keypoints_and_landmarks(-90,0,0)
+	p.apply_height_threshold(10,10)
+	p.apply_path_threshold(0.4)
+
+
+	p.plot_keyframes()
+	p.plot_landmarks()
+
+	# p.animate_keyframes(0.1);
+
+
+
+
+	plt.show()
+
+
+
+
+
